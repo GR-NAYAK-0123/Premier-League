a03227a425b5c81b19ce7efe16e7ff0025f6e594
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-02-06 01:33:43 +0530

    Added One Get API which will return the players based on the inputs

--- src/main/java/com/ipl/premier_league/controller/PlayerController.java
+++ src/main/java/com/ipl/premier_league/controller/PlayerController.java
@@ -5,20 +5,36 @@ import com.ipl.premier_league.service.PlayerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/players")
+@RequestMapping(path = "/api/players")
 public class PlayerController {
 
     private final PlayerService playerService;
 
     @GetMapping
-    public List<Player> getAllPlayer(){
-        return playerService.getAllPlayers();
+    public List<Player> getPlayers(@RequestParam(required = false) String name,
+                                   @RequestParam(required = false) String team,
+                                   @RequestParam(required = false) String role,
+                                   @RequestParam(required = false) String nation){
+        if (team != null && role != null){
+            return playerService.getAllPlayerByTeamAndRole(team, role);
+        } else if (name != null) {
+            return playerService.getAllPlayersByName(name);
+        } else if (role != null) {
+            return playerService.getAllPlayersByRole(role);
+        } else if (nation != null) {
+            return playerService.getAllPlayerByNation(nation);
+        } else if (team != null) {
+            return playerService.getPlayersByTeam(team);
+        } else {
+            return playerService.getAllPlayers();
+        }
     }
 
 }
