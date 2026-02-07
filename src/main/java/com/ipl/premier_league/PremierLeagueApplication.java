508c53595b83a013cff094eea4e1c42e3b0c80d4
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-02-07 19:33:06 +0530

    Again deleted those bug check

--- src/main/java/com/ipl/premier_league/PremierLeagueApplication.java
+++ src/main/java/com/ipl/premier_league/PremierLeagueApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PremierLeagueApplication {
 
 	public static void main(String[] args) {
-		String mongoUri = System.getenv("MONGO_URI");
-		System.out.println("MONGO_URI: " + mongoUri);
 		SpringApplication.run(PremierLeagueApplication.class, args);
 	}
 
